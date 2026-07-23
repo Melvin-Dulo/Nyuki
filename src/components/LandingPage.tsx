739dafe488d783202d64bec0390424a70b1a6b70
Melvin-Dulo <dulojrmelvin2@gmail.com>
2026-07-23 17:58:40 +0300

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -162,10 +162,7 @@ export default function LandingPage({ onNavigate, onOpenAuth }: LandingPageProps
                 <span className="block text-3xl font-black text-stone-900">100% Secure</span>
                 <span className="text-xs font-semibold text-stone-500 uppercase tracking-wider">Tenant & Staff Isolation</span>
               </div>
-              <div>
-                <span className="block text-3xl font-black text-stone-900">KES 2,500</span>
-                <span className="text-xs font-semibold text-stone-500 uppercase tracking-wider">Lowest Entry Price/Mo</span>
-              </div>
+    
             </div>
 
           </div>
